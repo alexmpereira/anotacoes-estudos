19340815da00a825b70a9f918e736dd0582661ce
alexmpereira <alexmpprog@gmail.com>
2019-10-21 12:55:02 -0300

    migrando pasta de estudos flutter

---
+++ studies-flutter/flutter_app/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app in flutter'),
+        ),
+        body: Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+// #docregion RandomWordsState, RWS-class-only
+class RandomWordsState extends State<RandomWords> {
+  // #enddocregion RWS-class-only
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+  // #docregion RWS-class-only
+}
+// #enddocregion RandomWordsState, RWS-class-only
+
+// #docregion RandomWords
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
